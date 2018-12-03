ba5e181d08461722672b2d7b05249b1b41b67cf6
raineynw <raineynw@rose-hulman.edu>
2018-12-03 11:28:50 -0500

    done

--- src/m1r_accumulator_examples.py
+++ src/m1r_accumulator_examples.py
@@ -5,8 +5,8 @@ This module demonstrates the ACCUMULATOR pattern in three classic forms:
    IN GRAPHICS:   x = x + pixels
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Nelson Rainey.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 #
@@ -27,38 +27,38 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
 #          which of the following three choices would you use?
 #
 #            for k in range(r - 1):
-#            for k in range(r):
+#            for k in range(r): this one
 #            for k in range(r + 1):
 #
 #     b. If you want a loop that runs from 0 to s, inclusive,
 #          what expression would you use in the _____ below?
 #
-#            for k in range(_____):
+#            for k in range(s+1):
 #
 #     c. If you want a loop that runs from r to s, inclusive, assuming s >= r,
 #          what expression would you use in the _____ below?
 #
-#            for k in range(_____):
+#            for k in range(s+1-r):
 #
 #     d. If you want a loop that runs from (r + 4) to (s - 10),
 #          including the (r + 4) but not including the (s - 10),
 #          what expression would you use in the _____ below?
 #
-#            for k in range(_____):
+#            for k in range((s-10)-(r+4)):
 #
 #     e. The following code snippet attempts to return the number
 #          of integers from r to s, inclusive, whose cosines are positive.
 #          It has at least 5 distinct errors (one per line).  What are they?
-#
-#              for k in range(r - s):
-#                  count = 0
-#                  if math.cos(r) > 0:
-#                      count = 1
+#                                               count = 0
+#               for k in range(r - s):          for k in range(s+1-r)
+#                  count = 0                    if math.cos(k+r)>0:
+#                  if math.cos(r) > 0:              count = count +1
+#                      count = 1               return count
 #                  return count
 #
 #     f. The code in the "graphics accumulation" example below includes:
 #            for _ in range(n):
-#        What does the   _   (underscore) mean?
+#        What does the   _   (underscore) mean? it is just a varible you can ignore
 #
 #     g. The code in the "graphics accumulation" example below includes:
 #
@@ -76,13 +76,13 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
 #
 #              x = starting_point.x
 #              for k in range(n):
-#                  center = rg.Point(x + (k * diameter), y)
+#                  center = rg.Point(x + (k * diameter), y)         right
 #                  circle = rg.Circle(point, radius)
 #                  circle.attach_to(window)
 #
 #              x = starting_point.x
 #              for k in range(n):
-#                  center = rg.Point(x + (k * diameter), y)
+#                  center = rg.Point(x + (k * diameter), y)         wrong
 #                  circle = rg.Circle(point, radius)
 #                  circle.attach_to(window)
 #                  x = x + (2 * radius)
